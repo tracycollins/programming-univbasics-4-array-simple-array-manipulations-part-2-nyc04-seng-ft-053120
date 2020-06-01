49c016a4a9258df51d63ead5c22d9ee26dff2826
Tracy Collins <tc@threeceemedia.com>
2020-06-01 21:04:12 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array1, element)
+  array1.insert(4, element)
+end
+
+def using_uniq(array1)
+  array1.uniq
+end
+
+def using_flatten(array1)
+  array1.flatten
+end
+
+def using_delete(array1, string)
+  array1.delete(string)
+end
+
+def using_delete_at(array1, index)
+  array1.delete_at(index)
+end
